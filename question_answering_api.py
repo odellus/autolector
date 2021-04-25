4ed3ce9f43f5869d1a1e259eb7e85dcee5911609
odellus <odell.wood@gmail.com>
2021-04-25 00:16:06 -0700

    Fun Saturday night making NLP APIs for free.

--- question_answering_api.py
+++ question_answering_api.py
@@ -10,30 +10,38 @@ api = Api(app)
 
 # Load question answering model.
 def load_model():
+    '''
+    Create a transformers pipeline for question answering inference.
+    '''
     print(' * Loading model...')
     start = time.time()
     nlp = pipeline('question-answering', model='distilbert-base-cased-distilled-squad')
     print(f' * Model loaded in {time.time()-start} seconds!')
     return nlp
 
-# Do this outside of the QuestionAnsweringApi class to load only once.
-nlp = load_model()
-
 class QuestionAnsweringApi(Resource):
-    def get(self):
-        return {
-            'question':'The question to be asked.',
-            'context': 'The context for the question.'
-            }
+    '''
+    Class to describes get, post, etc.. methods of our RESTful
+    question answering API. Inheirits flask_restful.Resource.
+    '''
     def post(self):
+        '''
+        POST method. Body of POST request must be in JSON format with schema:
+            { question: Question being asked,
+              context: Document containing answer }
+        Returns a JSON object.
+        '''
         inputs = request.get_json(force=True)
         question, context = inputs.get('question'), inputs.get('context')
         result = nlp(question=question, context=context)
         answer = result['answer']
         return {'answer': answer}
 
+# api
 api.add_resource(QuestionAnsweringApi, '/')
 port = 5000
 
 if __name__ == '__main__':
+    # Do this outside of the QuestionAnsweringApi class to load only once.
+    nlp = load_model()
     app.run(debug=True, port=port)
